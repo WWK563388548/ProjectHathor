33e4f90f610eb0f7449f472a729094a473e8e8cc
WWK563388548 <wc563388548@gmail.com>
2018-08-03 17:06:58 +0900

    Revert "update style"
    This reverts commit 7f8aa3ba34132d992536587deba1c309a5a3d0e8.

--- src/app/components/activities/dashboard/DashBoard.jsx
+++ src/app/components/activities/dashboard/DashBoard.jsx
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Grid } from 'semantic-ui-react';
-import ActivityList from '../activitylist/ActivityList';
 
 class DashBoard extends React.Component {
     render() {
         return (
             <Grid>
                 <Grid.Column width={10}>
-                    <ActivityList />
+                    <h2>The Left Part</h2>
                 </Grid.Column>
                 <Grid.Column width={6}>
                     <h2>The Right Part</h2>
