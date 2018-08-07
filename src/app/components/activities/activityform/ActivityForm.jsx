749017af7c2c9d7b8b98e0b458e734635063f029
WWK563388548 <wc563388548@gmail.com>
2018-08-08 00:26:41 +0900

    Update ActivityForm to get input value

--- src/app/components/activities/activityform/ActivityForm.jsx
+++ src/app/components/activities/activityform/ActivityForm.jsx
@@ -3,31 +3,54 @@ import { Segment, Form, Button } from 'semantic-ui-react';
 
 class ActivityForm extends React.Component {
 
+  state = {
+    event: {
+      title: '',
+      date: '',
+      city: '',
+      location: '',
+      hostedBy: '',
+    }
+  }
+
+  onFormSubmit = (event) => {
+    event.preventDefault();
+    console.log(this.state.event);
+  }
+
+  onInputChange = (event) => {
+    const newEvent = this.state.event;
+    newEvent[event.target.name] = event.target.value;
+    this.setState({
+      event: newEvent
+    });
+  }
+  
   render() {
     const handleClose = this.props.onClick;
-
+    // 使用'ref'获取值，代表uncontroled form
     return (
       <Segment>
-        <Form>
+        <Form onSubmit={this.onFormSubmit}>
           <Form.Field>
             <label>活动主题</label>
-            <input placeholder="活动的主题" />
+            <input name='title' onChange={this.onInputChange} value={this.state.event.title} placeholder="活动的主题" />
           </Form.Field>
           <Form.Field>
             <label>活动日期</label>
-            <input type="date" placeholder="YYYY-MM-DD" />
+            <input name='date' onChange={this.onInputChange} value={this.state.event.date} type="date" placeholder="YYYY-MM-DD" />
           </Form.Field>
           <Form.Field>
             <label>城市</label>
-            <input placeholder="活动所在城市" />
+            <input name='city' onChange={this.onInputChange} value={this.state.event.city} placeholder="活动所在城市" />
           </Form.Field>
           <Form.Field>
             <label>地点</label>
-            <input placeholder="活动举办地点" />
+            <input name='location' onChange={this.onInputChange} value={this.state.event.location} placeholder="活动举办地点" />
           </Form.Field>
           <Form.Field>
             <label>组织者</label>
-            <input placeholder="输入活动组织者的名字" />
+            <input name='hostedBy' onChange={this.onInputChange} value={this.state.event.hostedBy} placeholder="输入活动组织者的名字" />
           </Form.Field>
           <Button positive type="submit">
             提交
