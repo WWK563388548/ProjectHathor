8f7205ac84f6a51eb101e7df48b1fb213ba7d528
WWK563388548 <wc563388548@gmail.com>
2018-08-19 16:51:56 +0900

    add signIn menu and sign out menu in navbar

---
+++ src/app/components/nav/navbar/SignInMenu.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { Menu, Image, Dropdown } from 'semantic-ui-react';
+
+const SignInMenu = () => {
+    return (
+        <Menu.Item position="right">
+          <Image avatar spaced="right" src='/assets/user.png' />
+          <Dropdown pointing="top left" text="Username">
+            <Dropdown.Menu>
+              <Dropdown.Item text="创建新活动" icon="plus" />
+              <Dropdown.Item text="我的活动" icon="calendar" />
+              <Dropdown.Item text="我的好友" icon="users" />
+              <Dropdown.Item text="我的资料" icon="user" />
+              <Dropdown.Item text="设置" icon="settings" />
+              <Dropdown.Item text="退出登录" icon="power" />
+            </Dropdown.Menu>
+          </Dropdown>
+        </Menu.Item>
+    );
+}
+
+export default SignInMenu;
