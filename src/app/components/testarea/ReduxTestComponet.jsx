b10738594587097bd93fbe87a0f2c59a2333f27e
WWK563388548 <wc563388548@gmail.com>
2018-08-23 21:05:19 +0900

    install redux and add test component

---
+++ src/app/components/testarea/ReduxTestComponet.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ReduxTestComponent extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Test Area</h1>
+            </div>
+        );
+    }
+}
+
+export default ReduxTestComponent;
