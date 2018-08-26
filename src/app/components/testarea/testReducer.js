185b51e84931afa85376f0965102aebb8fa9285a
WWK563388548 <wc563388548@gmail.com>
2018-08-26 11:00:06 +0900

    update testReducer

--- src/app/components/testarea/testReducer.js
+++ src/app/components/testarea/testReducer.js
@@ -1,9 +1,19 @@
+import { INCREMENT_COUNTER, DECREMENT_COUNTER } from "../testConstants";
+
 const initialState = {
-    data: "This is test reducer!!!!!"
+    data: 100
 }
 
 const testReducer = (state = initialState, action) => {
-    return state;
+    switch(action.type) {
+        // 我们不直接修改state，而是复制和修改state，并返回复制修改后的state
+        case INCREMENT_COUNTER:
+            return {...state, data: state.data + 899};
+        case DECREMENT_COUNTER:
+            return {...state, data: state.data - 1};
+        default:
+            return state;
+    }
 }
 
 export default testReducer;
