3deb95a852c90a2e8cd01e919b2575ddb70dc934
WWK563388548 <wc563388548@gmail.com>
2018-08-26 13:08:50 +0900

    connecting the actions to the component

--- src/app/components/testarea/ReduxTestComponet.jsx
+++ src/app/components/testarea/ReduxTestComponet.jsx
@@ -1,9 +1,16 @@
 import React from 'react';
 import { connect } from 'react-redux';
+import { Button } from 'semantic-ui-react';
+import { incrementCounter, decrementCounter } from './testActions';
 
 const mapState = (state) => ({
     data: state.test.data,
-})
+});
+
+const actions = {
+    incrementCounter,
+    decrementCounter
+}
 
 class ReduxTestComponent extends React.Component {
     render() {
@@ -11,9 +18,11 @@ class ReduxTestComponent extends React.Component {
             <div>
                 <h1>Test Area</h1>
                 <h2>The answer is: {this.props.data}</h2>
+                <Button onClick={this.props.incrementCounter} color='green' content='increment' />
+                <Button onClick={this.props.decrementCounter} color='red' content='decrement' />
             </div>
         );
     }
 }
 
-export default connect(mapState)(ReduxTestComponent);
+export default connect(mapState, actions)(ReduxTestComponent);
