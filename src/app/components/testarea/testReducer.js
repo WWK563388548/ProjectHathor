74ec5d2a57c4e13aa5e0f142668077c880602bf3
WWK563388548 <wc563388548@gmail.com>
2018-08-26 20:21:56 +0900

    change testReducer and import boilerplate code

--- src/app/components/testarea/testReducer.js
+++ src/app/components/testarea/testReducer.js
@@ -1,19 +1,32 @@
 import { INCREMENT_COUNTER, DECREMENT_COUNTER } from "../testConstants";
+// 导入 boilerplate code of reducer
+import { createReducer } from '../util/reducerUtil';
 
 const initialState = {
     data: 100
+};
+
+export const incrementCounter = (state, payload) => {
+    return {...state, data: state.data + 899};
 }
 
-const testReducer = (state = initialState, action) => {
-    switch(action.type) {
-        // 我们不直接修改state，而是复制和修改state，并返回复制修改后的state
-        case INCREMENT_COUNTER:
-            return {...state, data: state.data + 899};
-        case DECREMENT_COUNTER:
-            return {...state, data: state.data - 1};
-        default:
-            return state;
-    }
+export const decrementCounter = (state, payload) => {
+    return {...state, data: state.data - 1};
 }
 
-export default testReducer;
+// const testReducer = (state = initialState, action) => {
+//    switch(action.type) {
+//        // 我们不直接修改state，而是复制和修改state，并返回复制修改后的state
+//        case INCREMENT_COUNTER:
+//            return {...state, data: state.data + 899};
+//        case DECREMENT_COUNTER:
+//            return {...state, data: state.data - 1};
+//        default:
+//            return state;
+//    }
+// }
+
+export default createReducer(initialState, {
+    [INCREMENT_COUNTER]: incrementCounter,
+    [DECREMENT_COUNTER]: decrementCounter
+});
