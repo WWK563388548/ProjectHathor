a92367f3f5588926b7c6054eec62a92d2e9e8fc5
WWK563388548 <wc563388548@gmail.com>
2018-08-30 00:39:28 +0900

    complete detail sidebar of activity

--- src/app/components/activities/activitydetail/ActivityDetailSideBar.jsx
+++ src/app/components/activities/activitydetail/ActivityDetailSideBar.jsx
@@ -1,9 +1,40 @@
 import React from 'react';
+import { Segment, List, Item, Label } from 'semantic-ui-react';
 
 class ActiivityDetailSideBar extends React.Component {
     render() {
         return (
-            <h1>Side Bar</h1>
+            <div>
+                <Segment
+                    textAlign="center"
+                    style={{ border: 'none' }}
+                    attached="top"
+                    secondary
+                    inverted
+                    color="teal"
+                >
+                    2人参加
+                </Segment>
+                <Segment attached>
+                    <List relaxed divided>
+                        <Item style={{ position: 'relative' }}>
+                            <Label
+                                style={{ position: 'absolute' }}
+                                color="orange"
+                                ribbon="right"
+                            >
+                                创办者
+                            </Label>
+                            <Item.Image size="tiny" src="/assets/user.png" />
+                            <Item.Content verticalAlign="middle">
+                                <Item.Header as="h3">
+                                    <a>Weikai Wang</a>
+                                </Item.Header>
+                            </Item.Content>
+                        </Item>
+                    </List>
+                </Segment>
+            </div>
         );
     }
 }
