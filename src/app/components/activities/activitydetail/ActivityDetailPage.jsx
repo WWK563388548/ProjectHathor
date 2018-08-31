4f8dd71d5a2feb4677ddaa4fc9ceae5eb2052ae2
WWK563388548 <wc563388548@gmail.com>
2018-09-01 08:23:21 +0900

    connect detail page to store

--- src/app/components/activities/activitydetail/ActivityDetailPage.jsx
+++ src/app/components/activities/activitydetail/ActivityDetailPage.jsx
@@ -1,49 +1,42 @@
 import React from 'react';
 import { Grid } from 'semantic-ui-react';
+import { connect } from 'react-redux';
 import DetailHeader from './ActivityDetailHeader';
 import DetailInfo from './ActivityDetailInfo';
 import DetailChat from './ActivityDetailChat';
 import DetailSideBar from './ActivityDetailSideBar';
 
-const activity = {
-    
-    id: '1',
-    title: 'Flutter学习会',
-    date: '2018-08-04',
-    category: 'knowledge',
-    description:'学习如何使用Dart与Flutter开发跨平台的移动应用',
-    city: '东京, 日本',
-    location: "东京都中央区银座1-43-2歌舞伎座塔",
-    hostedBy: 'Weikai Wang',
-    hostPhotoURL: 'https://randomuser.me/api/portraits/men/20.jpg',
-    participants: [
-        {
-          id: '1',
-          name: 'Weikai Wang',
-          photoURL: 'https://randomuser.me/api/portraits/men/20.jpg'
-        },
-        {
-          id: '2',
-          name: 'Kejun Chen',
-          photoURL: 'https://randomuser.me/api/portraits/men/22.jpg'
-        }
-    ]
-};
+const  mapState = (state, ownProps) => {
+    // console.log(ownProps);
+    // console.log(state);
+    const activityId = ownProps.match.params.id;
+    let activity = {};
+
+    if(activityId && state.activities.length > 0) {
+        activity = state.activities.filter(item => 
+            item.id === activityId
+        )[0];
+    }
+
+    // console.log(activity);
+    return activity;
+}
 
 // This is stateless functional component
-const ActivityDetailPage = () => {
+const ActivityDetailPage = (props) => {
+    // console.log(props);
     return (
         <Grid>
             <Grid.Column width={10}>
-                <DetailHeader activity={activity}/>
-                <DetailInfo activity={activity}/>
+                <DetailHeader activity={props}/>
+                <DetailInfo activity={props}/>
                 <DetailChat />
             </Grid.Column>
             <Grid.Column width={6}>
-                <DetailSideBar participants={activity.participants}/>
+                <DetailSideBar participants={props.participants}/>
             </Grid.Column>
         </Grid>
     );
 }
 
-export default ActivityDetailPage;
+export default connect(mapState)(ActivityDetailPage);
