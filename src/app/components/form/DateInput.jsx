eae5127ac536c1ed0b25ade01720c8386e2641b5
WWK563388548 <wc563388548@gmail.com>
2018-09-09 23:29:57 +0900

    update reusable date input field and it's style

--- src/app/components/form/DateInput.jsx
+++ src/app/components/form/DateInput.jsx
@@ -5,14 +5,15 @@ import 'react-datepicker/dist/react-datepicker.css';
 import moment from 'moment';
 
 // '...rest' 代表可以传入更多的属性(properties)
-const DateInput = ({input, width, placeholder, meta:{touched, error}, ...rest}) => {
+const DateInput = ({input: {value, onChange, ...restInput}, width, placeholder, meta:{touched, error}, ...rest}) => {
     return (
         <Form.Field error={touched && !!error} width={width}>
             <DatePicker
                 {...rest}
                 placeholderText={placeholder}
-                selected={input.value ? moment(input.value) : null}
-                onChange={input.onChange}
+                selected={value ? moment(value) : null}
+                onChange={onChange}
+                {...restInput}
             />
             {touched && error && <Label pointing color='red'>{error}</Label>}
         </Form.Field>
