a125d838a7ac11ef48725714e14e408d71360cae
WWK563388548 <wc563388548@gmail.com>
2018-09-16 18:53:33 +0900

    fix some default data

--- src/app/components/activities/activityReducer.jsx
+++ src/app/components/activities/activityReducer.jsx
@@ -11,6 +11,10 @@ const initialState = [
         '学习如何使用Dart与Flutter开发跨平台的移动应用',
       city: '东京, 日本',
       location: "东京都中央区银座1-43-2歌舞伎座塔",
+      locationLatLng: {
+        lat: 35.6697588,
+        lng: 139.76796439999998,
+      },
       hostedBy: 'Weikai Wang',
       hostPhotoURL: 'https://randomuser.me/api/portraits/men/20.jpg',
       participants: [
@@ -33,8 +37,12 @@ const initialState = [
       category: 'travel',
       description:
         '找伙伴去爬富士山，下山后也想去富士急',
-      city: '富士宫市, 日本',
-      location: '日本静冈县河口湖站',
+      city: '山梨县, 日本',
+      location: '日本山梨县河口湖站',
+      locationLatLng: {
+        lat: 35.498831,
+        lng: 138.76939400000003,
+      },
       hostedBy: 'Kejun Chen',
       hostPhotoURL: 'https://randomuser.me/api/portraits/men/22.jpg',
       participants: [
