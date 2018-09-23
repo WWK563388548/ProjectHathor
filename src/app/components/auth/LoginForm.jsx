71f222b4728e3a1840411a3e2767c05b73c0a85d
WWK563388548 <wc563388548@gmail.com>
2018-09-23 16:49:33 +0900

    set up loginForm

--- src/app/components/auth/LoginForm.jsx
+++ src/app/components/auth/LoginForm.jsx
@@ -1,30 +1,39 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { Form, Segment, Button } from 'semantic-ui-react';
 import { Field, reduxForm } from 'redux-form';
 import TextInput from '../form/TextInput';
+import { login } from './authActions';
 
-const LoginForm = () => {
-  return (
-    <Form error size="large">
-      <Segment>
-        <Field
-          name="email"
-          component={TextInput}
-          type="text"
-          placeholder="电子邮件地址"
-        />
-        <Field
-          name="password"
-          component={TextInput}
-          type="password"
-          placeholder="密码"
-        />
-        <Button fluid size="large" color="teal">
-          登陆
-        </Button>
-      </Segment>
-    </Form>
-  );
+const actions = {
+    login
+}
+
+const LoginForm = (props) => {
+    console.log("login form props: ", props);
+    const {handleSubmit} = props;
+    const {login} = props;
+    return (
+        <Form error size="large" onSubmit={handleSubmit(login)}>
+            <Segment>
+                <Field
+                name="email"
+                component={TextInput}
+                type="text"
+                placeholder="电子邮件地址"
+                />
+                <Field
+                name="password"
+                component={TextInput}
+                type="password"
+                placeholder="密码"
+                />
+                <Button fluid size="large" color="teal">
+                登陆
+                </Button>
+            </Segment>
+        </Form>
+    );
 };
 
-export default reduxForm({form: 'loginForm'})(LoginForm);
+export default connect(null, actions)(reduxForm({form: 'loginForm'})(LoginForm));
