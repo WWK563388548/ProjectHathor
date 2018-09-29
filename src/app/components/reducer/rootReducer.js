d00ff6e7ffe89f0f5499aeecbaf267e88b802645
WWK563388548 <wc563388548@gmail.com>
2018-09-29 10:57:10 +0900

    create asyncReducer

--- src/app/components/reducer/rootReducer.js
+++ src/app/components/reducer/rootReducer.js
@@ -3,12 +3,14 @@ import activityReducer from '../activities/activityReducer';
 import { reducer as FormReducer } from 'redux-form';
 import modalReducer from '../modals/modalReducer';
 import authReducer from '../auth/authReducer';
+import asyncReducer from '../async/asyncReducer';
 
 const rootReducer = combineReducers({
     form: FormReducer,
     activities: activityReducer,
     modals: modalReducer,
     auth: authReducer,
+    async: asyncReducer,
 });
 
 export default rootReducer;
