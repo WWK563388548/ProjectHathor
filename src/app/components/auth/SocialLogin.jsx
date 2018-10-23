96aee75af6309f6786046d9cdb6ad2405f53144f
WWK563388548 <wc563388548@gmail.com>
2018-10-23 23:17:47 +0900

    create SocialLogin component

---
+++ src/app/components/auth/SocialLogin.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Button, Icon } from 'semantic-ui-react';
+
+const SocialLogin = () => {
+    return (
+        <div>
+
+            <Button type="button" style={{ marginBottom: '10px' }} fluid color="facebook">
+                <Icon name="facebook" /> Login with Facebook
+            </Button>
+
+            <Button type="button" fluid color="google plus">
+                <Icon name="google plus" />
+                Login with Google
+            </Button>
+        </div>
+    );
+}
+
+export default SocialLogin;
