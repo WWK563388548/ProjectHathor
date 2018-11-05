beb21e7facbe22d72248833eef6a23f00f225222
WWK563388548 <wc563388548@gmail.com>
2018-11-06 00:29:52 +0900

    create PhotosPage.jsx

--- src/app/components/user/settings/PhotosPage.jsx
+++ src/app/components/user/settings/PhotosPage.jsx
@@ -1,12 +1,49 @@
-import React from 'react';
+import React, {Component} from 'react';
+import {Image, Segment, Header, Divider, Grid, Button, Card} from 'semantic-ui-react';
 
-// This is stateless functional component
-const PhotosPage = () => {
-    return (
-        <div>
-            <h1>Photos Page</h1>
-        </div>
-    );
+class PhotosPage extends Component {
+    render() {
+        return (
+            <Segment>
+                <Header dividing size='large' content='你的图片' />
+                <Grid>
+                    <Grid.Row />
+                    <Grid.Column width={4}>
+                        <Header color='teal' sub content='第一步 - 添加图片'/>
+                    </Grid.Column>
+                    <Grid.Column width={1} />
+                    <Grid.Column width={4}>
+                        <Header sub color='teal' content='第二步 - 裁剪图片' />
+                    </Grid.Column>
+                    <Grid.Column width={1} />
+                    <Grid.Column width={4}>
+                        <Header sub color='teal' content='第三步 - 预览与上传图片' />
+                    </Grid.Column>
+
+                </Grid>
+
+                <Divider/>
+                <Header sub color='teal' content='所有的图片'/>
+
+                <Card.Group itemsPerRow={5}>
+                    <Card>
+                        <Image src='https://randomuser.me/api/portraits/men/20.jpg'/>
+                        <Button positive>头像</Button>
+                    </Card>
+
+                        <Card >
+                            <Image
+                                src='https://randomuser.me/api/portraits/men/20.jpg'
+                            />
+                            <div className='ui two buttons'>
+                                <Button basic color='green'>设置</Button>
+                                <Button basic icon='trash' color='red' />
+                            </div>
+                        </Card>
+                </Card.Group>
+            </Segment>
+        );
+    }
 }
 
 export default PhotosPage;
