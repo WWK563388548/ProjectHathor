fb81b508c2d6366072657c0f5418d896ca32c9e4
WWK563388548 <wc563388548@gmail.com>
2018-11-17 10:47:45 +0900

    create photo upload method

--- src/app/components/user/userAction.jsx
+++ src/app/components/user/userAction.jsx
@@ -20,4 +20,46 @@ export const updateProfile = (user) =>
         } catch(error) {
             console.log(error);
         }
-    }
+    };
+
+export const uploadProfileImage = (file, fileName) => 
+    async (dispatch, getState, {getFirebase, getFirestore}) => {
+        const firebase = getFirebase();
+        const firestore = getFirestore();
+        const user = firebase.auth().currentUser;
+        const path = `${user.uid}/user_images`;
+        const options = {
+            name: fileName,
+        };
+
+        try {
+            // upload the file to the firebase storage
+            let uploadedFile = await firebase.uploadFile(path, file, null, options);
+            // get url of image
+            let downloadUrl = await uploadedFile.uploadTaskSnapshot.downloadUrl;
+            // get userdoc
+            let userDoc = await firestore.get(`users/${user.uid}`);
+            // check if user has photo, if not update profile with new image
+            if(!userDoc.data().photoURL){
+                await firebase.updateProfile({
+                    photoURL: downloadUrl,
+                });
+                await user.updateProfile({
+                    photoURL: downloadUrl,
+                });
+
+            }
+            // add the new photo as the new image in photos collection
+            return await firestore.add({
+                collection: 'users',
+                doc: user.uid,
+                subcollections: [{collection: 'photos'}]
+            }, {
+                name: fileName,
+                url: downloadUrl,
+            });
+        } catch (error) {
+            console.log(error);
+            throw new Error("上传图片时发生问题");
+        }
+    };
