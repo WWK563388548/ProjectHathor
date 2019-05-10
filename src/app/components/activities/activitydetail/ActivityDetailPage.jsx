0c34b7216b83f7a25553e7b642f778bff814a0d8
WWK563388548 <wc563388548@gmail.com>
2019-05-10 18:40:45 +0900

    Getting data from firestore for detail page

--- src/app/components/activities/activitydetail/ActivityDetailPage.jsx
+++ src/app/components/activities/activitydetail/ActivityDetailPage.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
+import React, {Component} from 'react';
 import { Grid } from 'semantic-ui-react';
 import { connect } from 'react-redux';
 import DetailHeader from './ActivityDetailHeader';
 import DetailInfo from './ActivityDetailInfo';
 import DetailChat from './ActivityDetailChat';
 import DetailSideBar from './ActivityDetailSideBar';
+import { withFirestore } from 'react-redux-firebase';
 
 const  mapState = (state, ownProps) => {
-    // console.log(ownProps);
-    // console.log(state);
+    console.log(ownProps);
+    console.log(state);
     const activityId = ownProps.match.params.id;
     let activity = {};
 
@@ -18,28 +19,46 @@ const  mapState = (state, ownProps) => {
         )[0];
     }
 
-    // console.log(activity);
+    console.log(activity);
     return {
         activity
     };
 }
 
-// This is stateless functional component
-// activity = this.props.activity
-const ActivityDetailPage = ({activity}) => {
-    console.log(this.props);
-    return (
-        <Grid>
-            <Grid.Column width={10}>
-                <DetailHeader activity={activity}/>
-                <DetailInfo activity={activity}/>
-                <DetailChat />
-            </Grid.Column>
-            <Grid.Column width={6}>
-                <DetailSideBar participants={activity.participants}/>
-            </Grid.Column>
-        </Grid>
-    );
+class ActivityDetailPage extends Component {
+
+
+    async componentDidMount() {
+        console.log("firestore props", this.props);
+        const {firestore, match} = this.props;
+        let activity = await firestore.get(`activities/${match.params.id}`);
+        console.log("detail page firestore activity", activity);
+    }
+
+    render() {
+        console.log("DetailPage", this.props);
+        const {activity} = this.props;
+        return (
+            <Grid>
+            {/*
+                <Grid.Column width={10}>
+                    <DetailHeader 
+                        // activity={activity}
+                    />
+                    <DetailInfo 
+                        // activity={activity}
+                    />
+                    <DetailChat />
+                </Grid.Column>
+                <Grid.Column width={6}>
+                    <DetailSideBar 
+                        // participants={activity.participants}
+                    />
+                </Grid.Column>
+            */}
+            </Grid>
+        );
+    }
 }
 
-export default connect(mapState)(ActivityDetailPage);
+export default withFirestore(connect(mapState)(ActivityDetailPage));
