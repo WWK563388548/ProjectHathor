f4a6935f9674b694c3a5a7aa2ace9d0b4dc31de3
WWK563388548 <wc563388548@gmail.com>
2019-05-14 22:27:27 +0900

    fix detail page  error

--- src/app/components/activities/activitydetail/ActivityDetailPage.jsx
+++ src/app/components/activities/activitydetail/ActivityDetailPage.jsx
@@ -7,13 +7,14 @@ import DetailChat from './ActivityDetailChat';
 import DetailSideBar from './ActivityDetailSideBar';
 import { withFirestore } from 'react-redux-firebase';
 import { toastr } from 'react-redux-toastr';
+import { objectToArray } from '../activityActions';
 
 const  mapState = (state) => {
-    // console.log(state);
+    // console.log("the detail page 1", state);
     let activity = {};
 
     if(state.firestore.ordered.activities && state.firestore.ordered.activities[0]) {
-        activity = state.firestore.ordered.activities[0]
+        activity = state.firestore.ordered.activities.filter(a => location.href.indexOf(a.id) > 0)[0]; // eslint-disable-line
     }
 
     // console.log(activity);
@@ -28,7 +29,7 @@ class ActivityDetailPage extends Component {
     async componentDidMount() {
         // console.log("firestore props", this.props);
         const {firestore, match, history} = this.props;
-        let activity = await firestore.get(`activities/${match.params.id}`);
+        let activity = await firestore.get(`/activities/${match.params.id}`);
         console.log("detail page firestore activity", activity);
         if(!activity.exists){
             history.push('/activities');
@@ -39,6 +40,7 @@ class ActivityDetailPage extends Component {
     render() {
         console.log("DetailPage", this.props);
         const {activity} = this.props;
+        const participants = activity && activity.participants && objectToArray(activity.participants);
         return (
             <Grid>
                 <Grid.Column width={10}>
@@ -52,7 +54,7 @@ class ActivityDetailPage extends Component {
                 </Grid.Column>
                 <Grid.Column width={6}>
                     <DetailSideBar 
-                        participants={activity.participants}
+                        participants={participants}
                     />
                 </Grid.Column>
             </Grid>
