b1b4c80fd1b3785542ed7bb36288c87e6d0f2e83
WWK563388548 <wc563388548@gmail.com>
2019-06-30 21:45:15 +0900

    add function of can see others profile

--- src/app/components/user/userdetail/UserDetailPage.jsx
+++ src/app/components/user/userdetail/UserDetailPage.jsx
@@ -1,32 +1,37 @@
-import React, {Component} from 'react';
-import {Grid} from 'semantic-ui-react';
-import {connect} from 'react-redux';
-import {firestoreConnect} from 'react-redux-firebase';
-import {compose} from 'redux';
+import React, { Component } from 'react';
+import { Grid } from 'semantic-ui-react';
+import { connect } from 'react-redux';
+import { firestoreConnect, isEmpty } from 'react-redux-firebase';
+import { compose } from 'redux';
 import UserDetailHeader from './UserDetailHeader';
 import UserDetailDescription from './UserDetailDescription';
 import UserDetailSidebar from './UserDetailSidebar';
 import UserDetailPhotos from './UserDetailPhotos';
-const query = ({auth}) => {
-    return [
-        {
-            collection: 'users',
-            doc: auth.uid,
-            subcollections: [{collection: 'photos'}],
-            storeAs: 'photos',
-        }
-    ];
-};
+import { userDetailedQuery } from '../userQueries';
 
-const mapState = (state) => ({
-    profile: state.firebase.profile,
-    auth: state.firebase.auth,
-    photos: state.firestore.ordered.photos,
-});
+const mapState = (state, ownProp) => {
+    let userUid = null;
+    let profile = {};
+
+    if(ownProp.match.params.id === state.auth.uid){
+        profile = state.firebase.profile;
+    } else {
+        profile = !isEmpty(state.firestore.ordered.profile) && state.firestore.ordered.profile[0];
+        userUid = ownProp.match.params.id;
+    }
+
+    return {
+        profile,
+        userUid,
+        auth: state.firebase.auth,
+        photos: state.firestore.ordered.photos,
+    };
+};
 
 class UserDetailedPage extends Component {
     render(){
         const {profile, photos, auth} = this.props;
+        console.log("UserDetailedPage", this.props);
         return (
             <Grid>
                 <UserDetailHeader profile={profile}/>
@@ -43,5 +48,5 @@ class UserDetailedPage extends Component {
 
 export default compose(
     connect(mapState),
-    firestoreConnect(auth => query(auth)),
+    firestoreConnect((auth, userUid) => userDetailedQuery(auth, userUid)),
 )(UserDetailedPage);
