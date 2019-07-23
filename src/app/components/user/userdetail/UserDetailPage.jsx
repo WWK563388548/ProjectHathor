7301eb669e25138972d1b7c55a9acee0d4ac7c83
WWK563388548 <wc563388548@gmail.com>
2019-07-23 23:36:50 +0900

    fix bugs and re-adding UserDetailsPhotos component

--- src/app/components/user/userdetail/UserDetailPage.jsx
+++ src/app/components/user/userdetail/UserDetailPage.jsx
@@ -12,8 +12,17 @@ import { userDetailedQuery } from '../userQueries';
 const mapState = (state, ownProp) => {
     let userUid = null;
     let profile = {};
-
-    if(ownProp.match.params.id === state.auth.uid){
+    let authUid = null;
+    // console.log("state.firebase.profile userDetailPage -1", state);
+    // console.log("state.firebase.profile userDetailPage 0", ownProp.match.params.id);
+    // console.log("state.firebase.profile userDetailPage 1", state.auth.uid);
+    if(state.auth.uid){
+        authUid = state.auth.uid;
+    } else {
+        authUid = state.firebase.auth.uid;
+    }
+    if(ownProp.match.params.id === authUid){
+        // console.log("state.firebase.profile userDetailPage", state.firebase.profile);
         profile = state.firebase.profile;
     } else {
         profile = !isEmpty(state.firestore.ordered.profile) && state.firestore.ordered.profile[0];
@@ -38,8 +47,8 @@ class UserDetailedPage extends Component {
                 <UserDetailHeader profile={profile}/>
                 <UserDetailDescription auth={auth} profile={profile}/>
                 <UserDetailSidebar isCurrentUser={isCurrentUser} />
-                {//photos && photos.length > 0 &&
-                    // <UserDetailPhotos photos={photos} />
+                {photos && photos.length > 0 &&
+                    <UserDetailPhotos photos={photos} />
                 }
             </Grid>
         );
